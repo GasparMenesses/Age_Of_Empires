88c42f74fe15957ae6b6e67f08a5a6211516e837
Gaspar Menesses <menessesgaspar@gmail.com>
2025-05-26 20:43:08 -0300

    modelo mapa

--- src/Library/Map.cs
+++ src/Library/Map.cs
@@ -2,5 +2,32 @@
 
 public class Map
 {
-    
+    public string[][] Board;
+
+    public Map()
+    {
+        Board = new string[100][];
+
+        for (int i = 0; i < 100; i++)
+        {
+            Board[i] = new string[100];
+
+            for (int j = 0; j < 100; j++)
+            {
+                Board[i][j] = "."; // Valor por defecto
+            }
+        }
+    }
+
+    // public void Imprimir()   ESTO NO VA ACÁ, NO ES SRP PERO FUNCA
+    // {
+    //     for (int i = 0; i < 100; i++)
+    //     {
+    //         for (int j = 0; j < 100; j++)
+    //         {
+    //             Console.Write(Board[i][j] + " ");
+    //         }
+    //         Console.WriteLine(); // Nueva línea al terminar una fila
+    //     }
+    // }
 }
