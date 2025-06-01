a082ca86ee5e1c5cf52424fff8549599630a1817
Gaspar Menesses <menessesgaspar@gmail.com>
2025-06-01 19:51:24 -0300

    map y mapprinter

--- src/Library/Core/Map.cs
+++ src/Library/Core/Map.cs
@@ -1,33 +1,12 @@
-﻿namespace Library;
-
-public class Map
+﻿public class Map
 {
-    public string[][] Board;
+    public string[,] Board;
 
     public Map()
     {
-        Board = new string[100][];
-
+        Board = new string[100, 100];
         for (int i = 0; i < 100; i++)
-        {
-            Board[i] = new string[100];
-
-            for (int j = 0; j < 100; j++)
-            {
-                Board[i][j] = "."; // Valor por defecto
-            }
-        }
+             for (int j = 0; j < 100; j++)
+                     Board[i, j] = ".";
     }
-
-    // public void Imprimir()   ESTO NO VA ACÁ, NO ES SRP PERO FUNCA
-    // {
-    //     for (int i = 0; i < 100; i++)
-    //     {
-    //         for (int j = 0; j < 100; j++)
-    //         {
-    //             Console.Write(Board[i][j] + " ");
-    //         }
-    //         Console.WriteLine(); // Nueva línea al terminar una fila
-    //     }
-    // }
 }
