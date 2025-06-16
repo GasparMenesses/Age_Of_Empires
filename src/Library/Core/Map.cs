506ea508c21612e7397bb05a872607e7d9c9bf17
Agustin Pechi <AGUSTIN.PECHI@correo.ucu.edu.uy>
2025-06-16 19:49:41 -0300

    Guardo mis cambios en Map

--- src/Library/Core/Map.cs
+++ src/Library/Core/Map.cs
@@ -1,12 +1,17 @@
 ﻿public class Map
 {
     public string[,] Board;
-
+    
     public Map()
     {
         Board = new string[100, 100];
         for (int i = 0; i < 100; i++)
              for (int j = 0; j < 100; j++)
                      Board[i, j] = ".";
+    }
+
+    public string CheckMap(int x, int y)
+    {
+        return Board[x, y];
     }
 }
