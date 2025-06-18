013070a6b54cb68a270bb75d31c0228345e97998
Agustin Pechi <AGUSTIN.PECHI@correo.ucu.edu.uy>
2025-06-18 19:18:07 -0300

    Depresion

--- src/Library/Core/Map.cs
+++ src/Library/Core/Map.cs
@@ -1,20 +1,17 @@
-﻿namespace Library.Core;
+﻿using Library;
 
 public class Map
 {
-    public static string[,] Board;
+    public string[,] Board;
     public Map()
     {
-        if (Board == null)
-        {
-            Board = new string[100, 100];
-            for (int i = 0; i < 100; i++)
-            for (int j = 0; j < 100; j++)
-                Board[i, j] = ".";
-        }
+        Board = new string[100, 100];
+        for (int i = 0; i < 100; i++)
+             for (int j = 0; j < 100; j++)
+                     Board[i, j] = ".";
     }
 
-    public static string CheckMap(int x, int y)
+    public string CheckMap(int x, int y)
     {
         return Board[x, y];
     }
