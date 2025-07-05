a1e946e8658647fba5694ab3d92b0de4b211a92f
Joaquin de Dios <joaquin.dedios@correo.ucu.edu.uy>
2025-07-05 20:17:18 -0300

    agrego tests

--- src/Library/Farming/Woods.cs
+++ src/Library/Farming/Woods.cs
@@ -6,7 +6,7 @@ namespace Library.Farming;
 public class Woods: Recolection
 {
     public static string Symbol => "Wd";
-    public Dictionary<string, int> Position { get; set; }
+    
 
     public Woods((int x, int y) posicion, int cantidadinicial)
         : base(posicion, cantidadinicial,120) 
